2d66a905d431dd94680fd08bf4f860b481e5c40c
Jeremy Twidt <jtwidt@gmail.com>
2025-10-15 21:56:41 -0500

    added report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sort_on
 
 def get_book_text(path_to_file):
   with open(path_to_file) as f:
@@ -10,7 +10,13 @@ def main():
   frankenstein_text = get_book_text("books/frankenstein.txt")
   num_words = get_word_count(frankenstein_text)
   character_counts = get_char_count(frankenstein_text)
+  character_counts.sort(key=sort_on, reverse=True)
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt")
+  print ("----------- Word Count ----------")
   print(f"Found {num_words} total words.")
-  print(f"Found the following character counts: {character_counts}")
+  print("-------- Character Count --------")
+  for counts in character_counts:
+    print(f"{counts['char']}: {counts['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -3,12 +3,24 @@ def get_word_count(text):
   return len(words)
 
 def get_char_count(text):
-  chars = {}
+  char_list = []
   words = text.split()
   for word in words:
     for i in range(0, len(word)):
       char = word[i].lower()
-      if char not in chars:
-        chars[char] = 0
-      chars[char] += 1
-  return chars
+      
+      found = False
+      
+      for j in range(0, len(char_list)):
+        if char_list[j]['char'] == char:
+          char_list[j]['num'] += 1
+          found = True
+          break
+      
+      if not(found):
+        char_list.append({'char': char, 'num': 1})
+
+  return char_list
+
+def sort_on(items):
+  return items["num"]
